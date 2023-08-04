5f021a6ff6c0aba944a5af890f3e46b5e7ee6355
wjdtjdwls95 <wjdtjdwls95@naver.com>
2023-08-04 21:21:02 +0900

    feat: 빠른시작 구현중

---
+++ Assets/Lobby/Scenes/SYJ/Scripts/PasswordRoomPanel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SYJ
+{
+
+    public class PasswordRoomPanel : MonoBehaviour
+    {
+        [SerializeField] public TMP_InputField passwordInput;
+        [SerializeField] public Button confirmBtn;
+    }
+}
---
+++ Assets/Lobby/Scenes/SYJ/Scripts/PlayerEntry.cs
@@ -0,0 +1,34 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SYJ
+{
+    public class PlayerEntry : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text playerName;
+        [SerializeField] private TMP_Text playerReady;
+        [SerializeField] private Button playerReadyButton;
+
+        private Player player;
+
+        public void Initialized(Player player)
+        {
+            this.player = player;
+            playerName.text = player.NickName;
+            playerReady.text = player.GetReady() ? "Ready" : "";
+            playerReadyButton.gameObject.SetActive(player.IsLocal);
+        }
+
+        public void OnReadyButtonClicked()
+        {
+            bool ready = player.GetReady();
+            ready = !ready;
+            player.SetReady(ready);
+        }
+    }
+}
